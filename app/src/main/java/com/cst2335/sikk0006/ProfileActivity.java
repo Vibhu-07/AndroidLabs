4e0c1d3b250fd41a7c3d9d17035b021adfee7f20
Vibhu Sikka <vibhu171202@gmail.com>
2023-02-16 07:10:06 -0500

    Finished lab 4

--- app/src/main/java/com/cst2335/sikk0006/ProfileActivity.java
+++ app/src/main/java/com/cst2335/sikk0006/ProfileActivity.java
@@ -1,23 +1,15 @@
 package com.cst2335.sikk0006;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
     public static final String TAG = "PROFILE_ACTIVITY";
@@ -26,77 +18,39 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Button button3 = findViewById(R.id.button3);
-        EditText emailEditText = findViewById(R.id.editTextTextEmailAddress);
-        Log.e(TAG, "In function: onCreate");
+        SharedPreferences shrd = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = shrd.edit();
+        Button save = findViewById(R.id.button3);
+        Button clear = findViewById(R.id.button4);
+        EditText name = findViewById(R.id.editTextTextPersonName2);
+        EditText address = findViewById(R.id.editTextTextEmailAddress);
+        TextView email = findViewById(R.id.textView4);
 
-        button3.setOnClickListener(new View.OnClickListener() {
+        Intent fromMain = getIntent();
+        String mail = fromMain.getStringExtra("EMAIL");
+        email.setText(mail);
+
+        name.setText(shrd.getString("Name",""));
+        address.setText(shrd.getString("Address", ""));
+        save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dispatchTakePictureIntent();
+                editor.putString("Name", name.getText().toString());
+                editor.putString("Address", address.getText().toString());
+                editor.apply();
             }
         });
 
-        Intent fromMain = getIntent();
-//        fromMain.getStringExtra("EMAIL");
-
-        emailEditText.setText(fromMain.getStringExtra("EMAIL"));
-    }
-
-        private void dispatchTakePictureIntent() {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                myPictureTakerLauncher.launch(takePictureIntent);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor.clear();
+                editor.apply();
             }
-        }
+        });
 
-        ActivityResultLauncher<Intent> myPictureTakerLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
-                            ImageView imgView = findViewById(R.id.imageView);
-                            imgView.setImageBitmap(imgbitmap);
-                        }
-                        else if(result.getResultCode() == Activity.RESULT_CANCELED) {
-                            Log.i(TAG, "User refused to capture a picture.");
-                        }
-                        Log.e(TAG, "In function: onActivityResult");
-                    }
-                } );
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.e(TAG, "In function: onStart");
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e(TAG, "In function: onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.e(TAG, "In function: onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e(TAG, "In function: onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e(TAG, "In function: onDestroy");
-    }
 
 }
 
